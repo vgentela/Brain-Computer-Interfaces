cde117574fc2518915aee8d7895b41d5e976b9c0
vgentela <vgentela@uvm.edu>
2024-04-24 19:49:02 -0400

    Correlations and features

--- test_project3.py
+++ test_project3.py
@@ -11,6 +11,8 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import eeglib.eeg as eeg
+import seaborn as sns
+import pandas as pd
 #%%
 def load_data(subject,directory = f'DASPS_Database/Raw data.mat/'):
     file_path = f'{directory}S{str(subject).rjust(2,"0") if subject < 10 else subject}.mat'
@@ -20,20 +22,33 @@ def load_data(subject,directory = f'DASPS_Database/Raw data.mat/'):
 #%%
 data,ham_scores,labels,situations = load_data(1)
 
+#%%
+def trials(data):
+    
+    trial_dic = {}
+    
+    for _ in range(len(data)):
+        trial_dic[f'trial_{_}'] = np.vstack(data[_])
+    
+    return trial_dic
+    
 #%% hjorth activity:
-def hjorth_params(data):
-    data = np.vstack([data[i][j:j+1] for j in range(len(data[0]))for i in range(len(data))])
-    #print(data)
+def hjorth_params(data_dic):
+    params = {}
     param_list = [eeg.hjorthActivity,eeg.hjorthComplexity,eeg.hjorthMobility]
-    params = np.array([[i(data.T[j]) for j in range(len(data.T))]for i in param_list])
-    return params.flatten()
+    for _ in range(len(data_dic)):
+        params[f'params_{_}'] = np.concatenate([np.apply_along_axis(i,0,data_dic.get(f'trial_{_}')) for i in param_list]).T
+    return params
 #%%
-params = hjorth_params(data)
+trial_dic = trials(data)
+params = hjorth_params(trial_dic)
 #%%
-len(params )
+df = pd.DataFrame(np.array(list(params.values())),index = params.keys())
+#%%
+df['valence'] = labels[0]
+df['arousal'] = labels[1]
+#%%
+corr = df.corr()
 
-
-
-
-
-
+sns.heatmap(corr)
+plt.savefig('correlation_heatmap')
