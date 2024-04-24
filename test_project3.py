7517a8e8b80fe9efbb31d54ac8d2b734cec210b8
jim1957 <jim.793hand@gmail.com>
2024-04-24 11:28:22 -0400

    Multiple subjects

--- test_project3.py
+++ test_project3.py
@@ -15,76 +15,84 @@ import h5py
 import matplotlib.pyplot as plt
 
 
-subject = 0 
-channel = 1
 
-#%% Load the data Processed Data
-filename = "S01preprocessed.mat"
+# #channel = 1
 
+# #%% Load the data Processed Data
+# # No need to use processed data, all the information is included in raw
+ 
+# filename = f'DASPS_Database/Preprocessed data .mat/S0{subject}preprocessed.mat'  #??? problem with leading zero 
 
-with h5py.File(filename, "r") as f:
-    # Print all root level object names (aka keys) 
-    # these can be group or dataset names 
-    print("Keys: %s" % f.keys())
-    # get first object name/key; may or may NOT be a group
-    a_group_key = list(f.keys())[0]
+# with h5py.File(filename, "r") as f:
+#     # Print all root level object names (aka keys) 
+#     # these can be group or dataset names 
+#     print("Keys: %s" % f.keys())
+#     # get first object name/key; may or may NOT be a group
+#     a_group_key = list(f.keys())[0]
 
-    # get the object type for a_group_key: usually group or dataset
-    print(type(f[a_group_key])) 
+#     # get the object type for a_group_key: usually group or dataset
+#     print(type(f[a_group_key])) 
 
-    # If a_group_key is a group name, 
-    # this gets the object names in the group and returns as a list
-    data = list(f[a_group_key])
+#     # If a_group_key is a group name, 
+#     # this gets the object names in the group and returns as a list
+#     data = list(f[a_group_key])
 
-    # If a_group_key is a dataset name, 
-    # this gets the dataset values and returns as a list
-    data = list(f[a_group_key])
-    # preferred methods to get dataset values:
-    ds_obj = f[a_group_key]      # returns as a h5py dataset object
-    ds_arr = f[a_group_key][()]  # returns as a numpy array 
+#     # If a_group_key is a dataset name, 
+#     # this gets the dataset values and returns as a list
+    
+#     # array [situations(recitation, imagine) x samples (1920) x channels (14)]
+    
+#     data = list(f[a_group_key])
+#     # preferred methods to get dataset values:
+#     ds_obj = f[a_group_key]      # returns as a h5py dataset object
+#     ds_arr = f[a_group_key][()]  # returns as a numpy array 
 
-#Processed data has only one key data ???
+# #Processed data has only one key data ???
 
-    # labels = list(h5py.File(filename, "r")['labels'])
-    # hamilton = list(h5py.File(filename, "r")['hamilton'])
-    # situation = list(h5py.File(filename, "r")['situation'])
+#     # labels = list(h5py.File(filename, "r")['labels'])
+#     # hamilton = list(h5py.File(filename, "r")['hamilton'])
+#     # situation = list(h5py.File(filename, "r")['situation'])
 
-#%% Visualize the data (Tiome and Frequency Domain)
-subject = 0 
-channel = 1
-fs = 128
-#Time Domain
-sample_size = len(ds_arr[subject,:,channel])
-t = np.arange(0,sample_size/fs,1/fs)
-plt.figure(num = 1, clear=all)
-plt.title(f"Processed Time Domain Subject {subject}")
-plt.ylabel("Amplitude (uV ?")
-plt.xlabel("Time (sec)")
-for channel in range(14):
-    print(f"channel {channel}")
-    plt.plot(t,ds_arr[subject,:,channel],label = (f"Chan {channel}"))
-plt.legend(loc='upper left')
-#Freq Domain
-freq = np.arange(0,((fs/2)+1/fs),1/15)
-PSD = 10*np.log10(np.fft.rfft(ds_arr[subject,:,channel])**2)
+# #%% Visualize the data (Time and Frequency Domain)
+# situation = 0   # There are 12 15 second situations (recitation, imagary) for now start with first
+# #channel = 1
+# fs = 128
+# #Time Domain
+# sample_size = len(ds_arr[subject,:,0])          #Assume same number of samples
+# t = np.arange(0,sample_size/fs,1/fs)
+# plt.figure(num = 1, clear=all)
+# plt.title(f"Processed Time Domain Subject {subject}")
+# plt.ylabel("Amplitude (uV ?")
+# plt.xlabel("Time (sec)")
+# for channel in range(14):
+#     print(f"channel {channel}")
+#     plt.plot(t,ds_arr[situation,:,channel],label = (f"Chan {channel}"))
+# plt.legend(loc='upper left')
+# #Freq Domain
+# freq = np.arange(0,((fs/2)+1/fs),1/15)
+# PSD = 10*np.log10(np.fft.rfft(ds_arr[situation,:,channel])**2)
 
-plt.figure(num = 2, clear=all)
-plt.title(f"Processed Frequency Domain Subject {subject}")
-plt.ylabel("PSD (dB)")
-plt.xlabel("Freq (Hz)")
-for channel in range(14):
-    print(f"channel {channel}")
-    plt.plot(freq,PSD,label = (f"Chan {channel}"))
-plt.legend(loc='upper right')
+# plt.figure(num = 2, clear=all)
+# plt.title(f"Processed Frequency Domain Subject {subject}")
+# plt.ylabel("PSD (dB)")
+# plt.xlabel("Freq (Hz)")
+# for channel in range(14):
+#     print(f"channel {channel}")
+#     plt.plot(freq,PSD,label = (f"Chan {channel}"))
+# plt.legend(loc='upper right')
 
 
 
 
 
 #%% Load the RAW data 
-filename = "S01.mat"
-
-with h5py.File(filename, "r") as f:
+subject = 6  # Hamilton sores = severe,  Arousal = 1
+Hamilton = 'Severe'
+subject = 9  # Hamilton sores = light ,  Arousal = 8
+Hamilton = 'Light'
+#filename = "DASPS_Database/Raw data.mat/S09.mat"   #DASPS_Database/Raw data.mat/S09.mat
+filename = f'DASPS_Database/Raw data.mat/S0{subject}.mat'  #??? problem with leading zero 
+with h5py.File(filename, "r") as f:  #DASPS_Database/Raw data.mat/S09.mat
     # Print all root level object names (aka keys) 
     # these can be group or dataset names 
     print("Keys: %s" % f.keys())
@@ -111,34 +119,41 @@ with h5py.File(filename, "r") as f:
 
 
 #%% Visualize the data (Time and Frequency Domain)
-
-
-subject = 0 
-channel = 1
+# loop through all 12 situations 6 runs x (recitation = recall)
 fs = 128
 #Time Domain
-sample_size = len(ds_arr[subject,:,channel])
+sample_size = len(ds_arr[subject,:,0]) # ??? assumes same size
 t = np.arange(0,sample_size/fs,1/fs)
-plt.figure(num = 3, clear=all)
-plt.title(f"Raw Time Domain Subject {subject}")
-plt.ylabel("Amplitude (uV ?")
-plt.xlabel("Time (sec)")
-for channel in range(14):
-    print(f"channel {channel}")
-    plt.plot(t,ds_arr[subject,:,channel],label = (f"Chan {channel}"))
-plt.legend(loc='upper left')
-#Freq Domain
-freq = np.arange(0,((fs/2)+1/fs),1/15)
-PSD = 10*np.log10(np.fft.rfft(ds_arr[subject,:,channel])**2)
-
-plt.figure(num = 4, clear=all)
-plt.title(f"Raw Frequency Domain Subject {subject}")
-plt.ylabel("PSD (dB)")
-plt.xlabel("Freq (Hz)")
-for channel in range(14):
-    print(f"channel {channel}")
-    plt.plot(freq,PSD,label = (f"Chan {channel}"))
-plt.legend(loc='upper right')
+num_of_situations = 2
+for situation in range (num_of_situations):
+    print(f"Situation {situation}")
+    #situation = 0 # the first step in the protocol
+    # fs = 128
+    # #Time Domain
+    # sample_size = len(ds_arr[subject,:,0])
+    # t = np.arange(0,sample_size/fs,1/fs)
+    plt.figure(num = situation, clear=all)
+    plt.suptitle(f"Raw Time Domain Subject {subject} Hamilton {Hamilton}")  #??? not loading this field correctly
+    plt.title(f"Situation {situation}, Valance = {labels[0][situation]}, Arousal ={labels[1][situation]} ")
+    plt.ylabel("Amplitude (uV ?")
+    plt.xlabel("Time (sec)")
+    for channel in range(14):
+        
+        plt.plot(t,ds_arr[situation,:,channel],label = (f"Chan {channel}"))
+    plt.legend(loc='upper left')
+    #Freq Domain
+    freq = np.arange(0,((fs/2)+1/fs),1/15)
+    PSD = 10*np.log10(np.fft.rfft(ds_arr[situation,:,channel])**2)
+    
+    plt.figure(num = num_of_situations+situation, clear=all)
+    plt.suptitle(f"Raw Frequency Domain Subject {subject}  Hamilton {Hamilton}")
+    plt.title(f"Situation {situation}, Valance = {labels[0][situation]}, Arousal ={labels[1][situation]} ")
+    plt.ylabel("PSD (dB)")
+    plt.xlabel("Freq (Hz)")
+    for channel in range(14):
+        
+        plt.plot(freq,PSD,label = (f"Chan {channel}"))
+    plt.legend(loc='upper right')
 
 
 
